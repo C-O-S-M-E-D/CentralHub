3d7fcf70ede63ebb3635bef4893aaeb89f918147
phuongho43 <phuongho43@gmail.com>
2016-07-19 17:40:50 -0700

    Starting DB output to patient profile

---
+++ Assets/Scripts/PatientProfileDataScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PatientProfileDataScript : MonoBehaviour {
+
+	public GameObject full_name_textbox;
+	public PatientDatabaseManager databaseManager;
+
+	public void SetInfo(string patient_name) {
+		databaseManager.GetPatientData(2);
+		this.full_name_textbox.GetComponent<Text>().text = ;
+	}
+
+}
