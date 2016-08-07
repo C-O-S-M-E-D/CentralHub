ca4f1d348920aeda35e72d71238afaa5f043f16c
phuongho43 <phuongho43@gmail.com>
2016-08-06 20:21:48 -0700

    Can now update patient data through profile by clicking text for left panel; Need to finish SetPatientData func and GetAndAddDialogInput for

--- Assets/Scripts/PatientDatabaseManager.cs
+++ Assets/Scripts/PatientDatabaseManager.cs
@@ -1,50 +1,24 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Data;
 using Mono.Data.Sqlite;
 using UnityEngine.UI;
-using System.Collections.Generic;
 
 
-public class PatientDatabaseManager : MonoBehaviour {
+public class PatientDatabaseManager {
 
 	private string connectionString;
 
-	//Patient Profile outputs
-	public Text id_textbox;
-	public Text fullname_textbox;
-	public Text gender_textbox;
-	public Text birthdate_textbox;
-	public Text weight_textbox;
-	public Text height_textbox;
-	public Text phone_textbox;
-	public Text appointment_textbox;
-	public Text latest_visit_date_textbox;
-	public Text latest_visit_doc_textbox;
+    public bool ConsistsOfWhiteSpace(string s){
+        foreach(char c in s){
+            if(c != ' ') return false;
+        }
+        return true;
+    }
 
-	//Patient Profile inputs
-    public Text firstname_input;
-    public Text lastname_input;
-	public GameObject gender_input;
-    public Text birthdate_input;
-    public Text weight_input;
-    public Text height_input;
-    public Text phone_input;
-    public Text latest_visit_doc_input;
-    public Text medications_input;
-    public Text conditions_input;
-
-	// Use this for initialization
-	void Start () {
-        
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-	}
-
+    // Retrieves id of latest patient: used for grabbing data for the profile page right after submitting the form
 	public string MostRecentPatient() {
         string last_patient_id = "";
 		connectionString = "URI=file:" + Application.dataPath + "/hubDB.db";
@@ -56,7 +30,7 @@ public class PatientDatabaseManager : MonoBehaviour {
 				using (IDataReader reader = dbCmd.ExecuteReader()) {
 					while (reader.Read()) {
                         last_patient_id = reader.GetInt64(0).ToString();
-						Debug.Log("MostRecentPatient was called: " + last_patient_id);
+						Debug.Log("MostRecentPatient: " + last_patient_id);
 					}
 					dbConnection.Close();
 					reader.Close();
@@ -66,7 +40,8 @@ public class PatientDatabaseManager : MonoBehaviour {
 		return last_patient_id;
 	}
 
-	private List<string> GetPatientData(string patient_id) {
+	public List<string> GetPatientData(string patient_id) {
+        // Dictionary might be better if order of DB columns get changed
 		List<string> patient_data = new List<string>();
 		connectionString = "URI=file:" + Application.dataPath + "/hubDB.db";
 		using (IDbConnection dbConnection = new SqliteConnection(connectionString)) {
@@ -79,7 +54,7 @@ public class PatientDatabaseManager : MonoBehaviour {
 						for (int i = 0; i < reader.FieldCount; i++) {
 							patient_data.Add(reader.GetValue(i).ToString());
 						}
-						Debug.Log("GetPatientData was called: " + patient_data[1]);
+						Debug.Log("GetPatientData: " + patient_data[1]);
 					}
 					dbConnection.Close();
 					reader.Close();
@@ -89,34 +64,33 @@ public class PatientDatabaseManager : MonoBehaviour {
 		return patient_data;
 	}
 
-	public void SetPatientData() {
-        string patient_id = MostRecentPatient();
-		List<string> data = GetPatientData(patient_id);
-		Debug.Log("SetPatientData was called with patient_id: " + patient_id);
-		this.id_textbox.GetComponent<Text>().text = "ID: " + data[0];
-		this.fullname_textbox.GetComponent<Text>().text = data[1] + " " + data[2];
-		this.gender_textbox.GetComponent<Text>().text = "Gender: " + data[3];
-		this.birthdate_textbox.GetComponent<Text>().text = "DoB: " + data[4];
-		this.weight_textbox.GetComponent<Text>().text = "Weight: " + data[5] + " Kg";
-		this.height_textbox.GetComponent<Text>().text = "Height: " + data[6] + " m";
-		this.phone_textbox.GetComponent<Text>().text = "Phone: " + data[7];
-		this.appointment_textbox.GetComponent<Text>().text = "Appt: " + data[8];
-		this.latest_visit_date_textbox.GetComponent<Text>().text = data[9];
-		this.latest_visit_doc_textbox.GetComponent<Text>().text = data[10];
-	}
-
-    private void InsertPatientData(string first_name, string last_name, string gender,
-        string birthdate, string weight, string height, string phone, string latest_visit_doc,
-		string medications, string conditions) {
+    public void InsertPatientData(string tableName, Dictionary<string, string> data) {
+        List<string> columns = new List<string>();
+        List<string> values = new List<string>();
+        // Exclude empty/whitespace strings where user didn't fill in the field
+        foreach (KeyValuePair<string, string> pair in data) {
+            if (ConsistsOfWhiteSpace(pair.Value)) {
+                
+            }
+            else {
+                columns.Add(pair.Key);
+                values.Add(pair.Value);
+            }
+        }
 		connectionString = "URI=file:" + Application.dataPath + "/hubDB.db";
 		using (IDbConnection dbConnection = new SqliteConnection(connectionString)) {
 			dbConnection.Open();
 			using (IDbCommand dbCmd = dbConnection.CreateCommand()) {
-				string sqlQuery = String.Format(@"INSERT INTO patients(first_name,
-                last_name,gender,date_of_birth,weight,height,phone_number,
-				latest_visit_doctor,medications,medical_conditions) 
-                VALUES('{0}','{1}','{2}','{3}',{4}, {5},'{6}','{7}','{8}','{9}')",
-                first_name, last_name, gender, birthdate, weight, height, phone, latest_visit_doc, medications, conditions);
+                string sqlQuery = "INSERT INTO " + tableName + " (" + columns[0];
+                for (int i = 1; i < columns.Count; i++) {
+                    sqlQuery += ", " + columns[i];
+                }
+                sqlQuery += ") VALUES (" + "'" + values[0] + "'";
+                for(int i = 1; i < values.Count; i++) {
+                    sqlQuery += ", " + "'" + values[i] + "'";
+                }
+                sqlQuery += ")";
+                Debug.Log("InsertPatientData: " + sqlQuery);
 				dbCmd.CommandText = sqlQuery;
 				dbCmd.ExecuteScalar();
 				dbConnection.Close();
@@ -124,28 +98,37 @@ public class PatientDatabaseManager : MonoBehaviour {
 		}
 	}
 
-    public void InsertPatientData() {
-		string first_name = this.firstname_input.GetComponent<Text>().text;
-		string last_name = this.lastname_input.GetComponent<Text>().text;
-		bool genderbool = this.gender_input.GetComponent<Toggle>().isOn;
-		string gender;
-		if (genderbool == true) {
-			gender = "M";
-		} else {
-			gender = "F";
-		}
-		string birth_date = this.birthdate_input.GetComponent<Text>().text;
-		string weight = this.weight_input.GetComponent<Text>().text;
-		string height = this.height_input.GetComponent<Text>().text;
-		string phone = this.phone_input.GetComponent<Text>().text;
-		string latest_visit_doc = this.latest_visit_doc_input.GetComponent<Text>().text;
-		string medications = this.medications_input.GetComponent<Text>().text;
-		string conditions = this.conditions_input.GetComponent<Text>().text;
+    public void UpdatePatientData(string tableName, Dictionary<string, string> data, string patient_id) {
+        List<string> columns = new List<string>();
+        List<string> values = new List<string>();
+        // Exclude empty/whitespace strings where user didn't fill in the field
+        foreach (KeyValuePair<string, string> pair in data) {
+            if (ConsistsOfWhiteSpace(pair.Value)) {
 
-        InsertPatientData(first_name,last_name,gender,birth_date,weight,height,phone,latest_visit_doc,medications,conditions);
+            }
+            else {
+                columns.Add(pair.Key);
+                values.Add(pair.Value);
+            }
+        }
+        connectionString = "URI=file:" + Application.dataPath + "/hubDB.db";
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString)) {
+            dbConnection.Open();
+            using (IDbCommand dbCmd = dbConnection.CreateCommand()) {
+                string sqlQuery = "UPDATE " + tableName + " SET " + columns[0] + "='" + values[0] + "'";
+                for (int i = 1; i < columns.Count; i++) {
+                    sqlQuery += ", " + columns[i] + "='" + values[i] + "'";
+                }
+                sqlQuery += " WHERE patient_id='" + patient_id + "'";
+                Debug.Log("UpdatePatientData: " + sqlQuery);
+                dbCmd.CommandText = sqlQuery;
+                dbCmd.ExecuteScalar();
+                dbConnection.Close();
+            }
+        }
     }
-
-	private void DeletePatientData (int patient_id) {
+        
+	public void DeletePatientData (int patient_id) {
 		connectionString = "URI=file:" + Application.dataPath + "/hubDB.db";
 		using (IDbConnection dbConnection = new SqliteConnection(connectionString)) {
 			dbConnection.Open();
@@ -157,5 +140,4 @@ public class PatientDatabaseManager : MonoBehaviour {
 			}
 		}
 	}
-
 }
---
+++ Assets/Scripts/PatientProfileManager.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using MaterialUI;
+
+public class PatientProfileManager : MonoBehaviour {
+
+    // Patient Profile outputs
+    public Text id_textbox;
+    public Text firstname_textbox;
+    public Text lastname_textbox;
+    public Text gender_textbox;
+    public Text birthdate_textbox;
+    public Text weight_textbox;
+    public Text height_textbox;
+    public Text phone_textbox;
+    public Text appointment_textbox;
+    public Text latest_visit_date_textbox;
+    public Text latest_visit_doc_textbox;
+
+    // Patient Profile inputs
+    private string firstname_input;
+    private string lastname_input;
+    private string gender_input;
+    private string birthdate_input;
+    private string weight_input;
+    private string height_input;
+    private string phone_input;
+    private string latest_visit_doc_input;
+    private string medications_input;
+    private string conditions_input;
+
+    // Passing List of Params to Dialog for Updating profiles
+    private string updateColumn;
+
+	// Reference to DB manager script to use methods
+    public PatientDatabaseManager manager = new PatientDatabaseManager();
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    // Empties the textboxes after inserting the data; Having issues with MaterialUI's text inputs...    
+//    public void ClearForm() {
+//        firstname_input.GetComponent<InputField>().text = string.Empty;
+//        lastname_input.GetComponent<InputField>().text = string.Empty;
+//        gender_input.GetComponent<InputField>().text = string.Empty;
+//        birthdate_input.GetComponent<InputField>().text = string.Empty;
+//        weight_input.GetComponent<InputField>().text = string.Empty;
+//        height_input.GetComponent<InputField>().text = string.Empty;
+//        phone_input.GetComponent<InputField>().text = string.Empty;
+//        latest_visit_doc_input.GetComponent<InputField>().text = string.Empty;
+//        medications_input.GetComponent<InputField>().text = string.Empty;
+//        conditions_input.GetComponent<InputField>().text = string.Empty;
+//        Debug.Log("ClearForm: " + firstname_input.GetComponent<InputField>().text);
+//    }
+
+    // Changes text on patient profile according to data from GetPatientData
+    public void SetProfileData(string patient_id) {
+        if (patient_id == "new") {
+            patient_id = manager.MostRecentPatient();
+        }
+        List<string> data = manager.GetPatientData(patient_id);
+        Debug.Log("SetProfileData: " + patient_id);
+        // Targetting very specific objects on the profile page
+        id_textbox.GetComponent<Text>().text = data[0];
+        firstname_textbox.GetComponent<Text>().text = data[1];
+        lastname_textbox.GetComponent<Text>().text = data[2];
+        gender_textbox.GetComponent<Text>().text = data[3];
+        birthdate_textbox.GetComponent<Text>().text = data[4];
+        weight_textbox.GetComponent<Text>().text = data[5];
+        height_textbox.GetComponent<Text>().text = data[6];
+        phone_textbox.GetComponent<Text>().text = data[7];
+        appointment_textbox.GetComponent<Text>().text = data[8];
+        latest_visit_date_textbox.GetComponent<Text>().text = data[9];
+        latest_visit_doc_textbox.GetComponent<Text>().text = data[10];
+    }
+
+    public void GetAndAddFormInput() {
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        // Targetting very specific input field objects on the form page
+        // Maybe use FindGameObjectsWithTag instead to get a list
+        firstname_input = GameObject.FindGameObjectWithTag("Firstname_Inputfield").GetComponent<InputField>().text.Trim();
+        lastname_input = GameObject.FindGameObjectWithTag("Lastname_Inputfield").GetComponent<InputField>().text.Trim();
+        gender_input = GameObject.FindGameObjectWithTag("Gender_Inputfield").GetComponent<InputField>().text.Trim();
+        birthdate_input = GameObject.FindGameObjectWithTag("DoB_Inputfield").GetComponent<InputField>().text.Trim();
+        weight_input = GameObject.FindGameObjectWithTag("Weight_Inputfield").GetComponent<InputField>().text.Trim();
+        height_input = GameObject.FindGameObjectWithTag("Height_Inputfield").GetComponent<InputField>().text.Trim();
+        phone_input = GameObject.FindGameObjectWithTag("Phone_Inputfield").GetComponent<InputField>().text.Trim();
+        medications_input = GameObject.FindGameObjectWithTag("Meds_Inputfield").GetComponent<InputField>().text.Trim();
+        conditions_input = GameObject.FindGameObjectWithTag("Conditions_Inputfield").GetComponent<InputField>().text.Trim();
+        latest_visit_doc_input = GameObject.FindGameObjectWithTag("Doc_Inputfield").GetComponent<InputField>().text.Trim();
+        data.Add("first_name", firstname_input);
+        data.Add("last_name", lastname_input);
+        data.Add("gender", gender_input);
+		// If using radio buttons
+//        bool malebool = this.gender_m_input.GetComponent<Toggle>().isOn;
+//        bool femalebool = this.gender_f_input.GetComponent<Toggle>().isOn;
+//        string gender;
+//        if (malebool == true) {
+//            gender = "M";
+//        } else if (femalebool == true) {
+//            gender = "F";
+//        } else {
+//            gender = "";
+//        }
+//        data.Add("gender", gender);
+        data.Add("date_of_birth", birthdate_input);
+        data.Add("weight", weight_input);
+        data.Add("height", height_input);
+        data.Add("phone_number", phone_input);
+        data.Add("latest_visit_doctor", latest_visit_doc_input);
+        data.Add("medications", medications_input);
+        data.Add("medical_conditions", conditions_input);
+        Debug.Log("AddPatientData: " + data["first_name"]);
+        // Addresses when the user enters nothing in the form
+        bool goAhead = false;
+        foreach (KeyValuePair<string, string> pair in data) {
+            if (!manager.ConsistsOfWhiteSpace(pair.Value)) {
+                goAhead = true;
+            }
+        }
+        if (goAhead) {
+            manager.InsertPatientData("patients", data);
+            Debug.Log("AddPatientData: at least one inputfield was filled out");
+        }
+    }
+
+    public void GetCorrespondingColumn() {
+        // Get tag of game object that was just clicked on
+        string objectTag = EventSystem.current.currentSelectedGameObject.tag;
+		Dictionary<string, string> TagToColumnName = new Dictionary<string, string>()
+		{
+			{"Firstname_ProfileText","first_name"},
+            {"Lastname_ProfileText","last_name"},
+			{"Gender_ProfileLabel","gender"},
+			{"DoB_ProfileLabel","date_of_birth"},
+			{"Weight_ProfileLabel","weight"},
+			{"Height_ProfileLabel","height"},
+			{"Phone_ProfileLabel","phone_number"},
+			{"Appt_ProfileLabel","appointment"},
+		};
+        Debug.Log("GetCorrespondingColumn: " + TagToColumnName[objectTag]);
+        updateColumn = TagToColumnName[objectTag];
+	}
+
+    public void GetAndAddDialogInput() {
+        string patient_id = id_textbox.GetComponent<Text>().text;
+        Dictionary<string,string> columnAndValue = new Dictionary<string,string>();
+        string updateValue = GameObject.FindGameObjectWithTag("UpdateProfileDialog_Inputfield").GetComponent<InputField>().text.Trim();
+        columnAndValue.Add(updateColumn, updateValue);
+        manager.UpdatePatientData("patients", columnAndValue, patient_id);
+        SetProfileData(patient_id);
+        GameObject.FindGameObjectWithTag("UpdateProfileDialog_Inputfield").GetComponent<InputField>().text = string.Empty;
+    }
+}
